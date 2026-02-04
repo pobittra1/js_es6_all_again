cfe899043c2c87e2653d4c6abda279dc8d705f33
pobittra chandra roy <devpobittra@gmail.com>
2026-02-04 15:00:05 +0600

    explore reduce func

---
+++ more_es6/reduce.js
@@ -0,0 +1,6 @@
+//here i will generate a array numbers in all of sum.
+const numbers = [1, 2, 3, 4, 5, 6];
+const sumOfAllNumbers = numbers.reduce((initial, singleValue) => initial + singleValue, 0)
+console.log(sumOfAllNumbers);
+//initial that's mean first value by default
+// need to be explore it again on doc.
