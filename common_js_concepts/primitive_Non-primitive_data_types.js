9ea6b77828b7dd2bde9bbfd5f40a1445db204ec1
pobittra chandra roy <devpobittra@gmail.com>
2026-02-07 09:56:58 +0600

    primtive and non-primitive data types identify

---
+++ common_js_concepts/primitive_Non-primitive_data_types.js
@@ -0,0 +1,35 @@
+//primitive data type
+//primitive data types can store only single value. like--
+
+//1. string
+let personName = "iri";
+console.log(typeof personName); // output = string.
+
+//2. number
+let personAge = 30;
+console.log(typeof personAge); // output =  number.
+
+//3. boolean
+let canWalk = true;
+console.log(typeof canWalk); // out = boolean.
+
+//4. undefined
+let haveMoney; // don't set any value here that's why it's undefined.
+console.log(typeof haveMoney);//output = undefined.
+
+//5. null
+let haveSon = null // that's mean don't have any son of this person.
+console.log(typeof haveSon);// output = object. it's speacial data type.
+
+
+
+//non-primitive data type
+//it can store many things. like--
+
+let person = { name: "tiri", age: 20, isMan: true };
+console.log(typeof person); // it's a object. they can store string, number, boolean or many type of values here.
+
+let friends = ['simu', "iri", "tiri", "siriya"];
+console.log(typeof friends); // it's an array but their type also an object. have special reason. also they can store many data types
+
+
