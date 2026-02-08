94434e2c5b6b7939dfcf4dfb042980a4182ca5f9
pobittra chandra roy <devpobittra@gmail.com>
2026-02-08 21:44:27 +0600

    recap json fetch() with posts and console each post data

--- api_json_dataLoad_dynamicWebsite/load-json-data-using-fetch-func.js
+++ api_json_dataLoad_dynamicWebsite/load-json-data-using-fetch-func.js
@@ -3,3 +3,20 @@ const loadData = () => { //onClick func
         .then(res => res.json()) //return promise
         .then(data => console.log(data)) //return promise again the return original data.
 }
+
+
+// recap fetch with posts and console each data
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data); // full json parse data will be loaded from here
+        displayPost(data); //send the data in displayPost func.
+    });
+}
+
+
+const displayPost = (posts) => {
+    console.log(posts); //display the full data from here
+    posts.forEach(post => console.log(post)); //display every array of obj data in single line.
+
+}
