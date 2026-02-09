788126450c02f7aa5f80ad746df62ba8d28e7291
pobittra chandra roy <devpobittra@gmail.com>
2026-02-09 10:44:35 +0600

    design the card after fetching this post data

--- api_json_dataLoad_dynamicWebsite/posts.js
+++ api_json_dataLoad_dynamicWebsite/posts.js
@@ -14,10 +14,19 @@ const displayPost = (posts) => {
     // getting the post container from html
     const postContainer = document.getElementById("postContainer");
     posts.forEach(post => {
-        //create li for every title and append it into post container.
-        const li = document.createElement("li");
-        li.innerText = post.title;
-        //append li into container.
-        postContainer.appendChild(li);
+        // //create li for every title and append it into post container.
+        // const li = document.createElement("li");
+        // li.innerText = post.title;
+        // //append li into container.
+        // postContainer.appendChild(li);
+        const postCard = document.createElement("div");
+        postCard.innerHTML = `
+          <div class="post-card">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-body">${post.body}</p>
+        </div>
+        `
+
+        postContainer.append(postCard);
     });
 }
